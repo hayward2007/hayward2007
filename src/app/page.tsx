4c8a9e8a56ca18fe1d7ecfeccf6dfc4f7c29f8ff
hayward2007 <hayward2007@naver.com>
2024-03-25 07:25:11 +0900

    Name tag animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,39 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import { useState } from "react";
+import styles from "./style.module.css";
 
 export default function Home() {
+  const [isHovering, setIsHovering] = useState(false);
+  const [isClicked, setIsClicked] = useState("HAYWARD KIM");
+
+  const mouseMove = (e: any) => {
+    const rect = e.target.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top; 
+    var rotateY = -1/100 * x + 5;
+    var rotateX = 1/10 * y - 15;
+    e.target.style = `transform : perspective(350px) rotateX(${rotateX}deg) rotateY(${rotateY}deg); scale : 125%;`;
+  }
+
+  const mouseLeave = (e: any) => {
+    setIsHovering(false);
+    e.target.style = `transform : perspective(350px) rotateX(0deg) rotateY(0deg); transition : 0.5s;`;
+  } 
+
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        {/* <div className={styles.edgeTopLeft}></div> */}
+        <div className={styles.name} onMouseEnter={() => setIsHovering(true)} onMouseMove={mouseMove} onMouseLeave={mouseLeave} onMouseUp={() => setIsClicked(isClicked == "HAYWARD KIM" ? "Kim Hyoung Seok" : "HAYWARD KIM")}>
+          {isClicked}
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        {/* <div className={styles.name} onMouseEnter={() => setIsHovering(true)} onMouseMove={mouseMove} onMouseLeave={mouseLeave} onMouseUp={() => setIsClicked(isClicked == "HAYWARD KIM" ? "Kim Hyoung Seok" : "HAYWARD KIM")}>
+          {isClicked}
+        </div>
+        <div className={styles.name} onMouseEnter={() => setIsHovering(true)} onMouseMove={mouseMove} onMouseLeave={mouseLeave} onMouseUp={() => setIsClicked(isClicked == "HAYWARD KIM" ? "Kim Hyoung Seok" : "HAYWARD KIM")}>
+          {isClicked}
+        </div> */}
+  </main>
   );
 }
